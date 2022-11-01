5501c3019922dcc4e8cce1e9e1c0d2181768a050
seongho <jklbnm2002@naver.com>
2022-11-01 17:46:08 +0900

    추가: 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import EditorPage from "./pages/Editor";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ViewerPage from "./pages/Viewer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/view">
+          <ViewerPage />
+        </Route>
+
+        <Route path="/edit">
+          <EditorPage />
+        </Route>
+
+        <Route path="/">
+          <ViewerPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
