cf452e34aa920840c28f6449103c4bc906b8e3e8
seongho <jklbnm2002@naver.com>
2022-11-01 17:46:30 +0900

    추가: 기본 컨테이너

---
+++ src/components/containers/Container.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+`;
+
+export { Container };
