e189d73212e34daca82c6157395cdc128f023586
seongho <jklbnm2002@naver.com>
2022-11-02 11:27:59 +0900

    추가: 참고링크

--- src/apis/utils/getBase64ToImgUrl.ts
+++ src/apis/utils/getBase64ToImgUrl.ts
@@ -7,3 +7,5 @@ export default async function getBlobToImgUrl(blob: Blob) {
   const response = await axios.post(`${PREFIX_UTIL_HOST}/images/file`, form);
   return response.data;
 }
+
+// 참고: https://forteleaf.tistory.com/entry/axiospost-%EC%97%90-form-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%A5%BC-%EB%84%A3%EA%B8%B0
